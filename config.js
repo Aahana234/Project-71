e8342d446699f714b11f1c67841593cf0048063a
Aahana234 <73110224+Aahana234@users.noreply.github.com>
2021-11-03 09:17:03 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+require('@firebase/firestore');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBlop8NJvVJ4i87NqRY-3c7fJ38GTwtlNQ",
+    authDomain: "complaint-forum---71.firebaseapp.com",
+    projectId: "complaint-forum---71",
+    storageBucket: "complaint-forum---71.appspot.com",
+    messagingSenderId: "749941493976",
+    appId: "1:749941493976:web:86a1bf92254b1991a707ad"
+  };
+  
+
+  firebase.initializeApp(firebaseConfig);
+  
+  export default firebase.firestore();
